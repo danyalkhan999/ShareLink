a69eacd6acb7f70afe440306f2db7a64569e790c
Danyal <danyalkhan8271@gmail.com>
2023-09-13 11:05:54 +0530

    logout

--- src/app/pages/sharelink/sharelink.component.ts
+++ src/app/pages/sharelink/sharelink.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-sharelink',
   templateUrl: './sharelink.component.html',
-  styleUrls: ['./sharelink.component.css']
+  styleUrls: ['./sharelink.component.css'],
 })
 export class SharelinkComponent {
-
+  constructor(private authService: AuthService) {}
+  logout() {
+    this.authService.logout();
+  }
 }
