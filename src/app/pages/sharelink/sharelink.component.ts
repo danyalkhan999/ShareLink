3d514837e0bc2389c233ebffa1c0ebe08d084ae5
Danyal <danyalkhan8271@gmail.com>
2023-09-13 11:20:31 +0530

    logout works

--- src/app/pages/sharelink/sharelink.component.ts
+++ src/app/pages/sharelink/sharelink.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
@@ -7,8 +8,9 @@ import { AuthService } from 'src/app/Services/auth.service';
   styleUrls: ['./sharelink.component.css'],
 })
 export class SharelinkComponent {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   logout() {
     this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
